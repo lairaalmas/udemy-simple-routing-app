21e734e8e1830f39fb2d2011453db8e25a73d009
lairaalmas <lairaalmas@gmail.com>
2023-03-01 21:03:48 -0300

    showing different approach to the creation of routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route
+} from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
