62c2ccbecc36cd51b253823d2c64f71632bdf51b
lairaalmas <lairaalmas@gmail.com>
2023-03-02 00:09:19 -0300

    add root layout and error handling

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import {
   // createRoutesFromElements,
   // Route
 } from "react-router-dom";
+import ErrorPage from "./pages/Error";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -17,8 +19,15 @@ import ProductsPage from "./pages/Products";
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 function App() {
