b01223def35d23754085c50cc95666f37d747a6f
lairaalmas <lairaalmas@gmail.com>
2023-03-02 00:34:47 -0300

    using NavLink instead of Link

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
@@ -8,10 +8,25 @@ const MainNavigation = () => {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="./">Home</Link>
+            <NavLink
+              to="./"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="./products">Products</Link>
+            <NavLink
+              to="./products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
