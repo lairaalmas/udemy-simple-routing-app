18c00d29cab66ebc202e2f003899613d765dd381
lairaalmas <lairaalmas@gmail.com>
2023-03-01 21:05:15 -0300

    creating links without sending new http request with Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <h1>My home page</h1>;
+  return (
+    <>
+      <h1>My home page</h1>
+      <p>
+        Go to <Link to="./products">the list of products</Link>
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
